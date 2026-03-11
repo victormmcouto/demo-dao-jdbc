cd1558e091c1d5026df01e4ae1fc132108d44abb
victormmcout <myprogrammingthings@gmail.com>
2026-03-10 21:52:27 -0300

    feat: Create Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		
 		System.out.println(obj);
-
+		System.out.println(seller);
 	}
 
 }
