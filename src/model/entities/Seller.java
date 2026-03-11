7ab89a26058fdb394364d2fb1d96e00d879739cf
victormmcout <myprogrammingthings@gmail.com>
2026-03-10 22:58:02 -0300

    feat: Implements findById in SellerDao

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,7 +1,7 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.sql.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable{
@@ -11,7 +11,7 @@ public class Seller implements Serializable{
 	private Integer id;
 	private String name;
 	private String email;
-	private LocalDate birthDate;
+	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
@@ -20,7 +20,7 @@ public class Seller implements Serializable{
 		
 	}
 
-	public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary,
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary,
 			Department department) {
 		super();
 		this.id = id;
@@ -55,11 +55,11 @@ public class Seller implements Serializable{
 		this.email = email;
 	}
 
-	public LocalDate getBirthDate() {
+	public Date getBirthDate() {
 		return birthDate;
 	}
 
-	public void setBirthDate(LocalDate birthDate) {
+	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
 
